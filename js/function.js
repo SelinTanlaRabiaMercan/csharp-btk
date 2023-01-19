8bee0ac1103114f3adf2431f966f351cc87c86a7
Selin TANLA <rabiamercan8@gmail.com>
2023-01-19 22:27:50 +0300

    neden fonksiyonlara ihtiyaç duyarız

---
+++ js/function.js
@@ -0,0 +1,8 @@
+//random() : random sayı : 0 ve 1 arasında üretir bu yüzden 49 ile çarpıyorum
+//ceil() : sayıyı yukarı yuvarlar
+var sayi =Math.ceil(Math.random()*49)
+var sayi1=Math.ceil(Math.random()*49)
+var sayi2=Math.ceil(Math.random()*49)
+var sayi3=Math.ceil(Math.random()*49)
+var sayi4=Math.ceil(Math.random()*49)
+console.log(sayi+' '+sayi1+' '+sayi2+' '+sayi3+' '+sayi4)
