d96d200b1d0cb6bbadacf0f860a65895183c2d8b
Selin TANLA <rabiamercan8@gmail.com>
2023-01-19 22:35:19 +0300

    ilk fonksiyon

--- js/function.js
+++ js/function.js
@@ -1,8 +1,13 @@
 //random() : random sayı : 0 ve 1 arasında üretir bu yüzden 49 ile çarpıyorum
 //ceil() : sayıyı yukarı yuvarlar
-var sayi =Math.ceil(Math.random()*49)
-var sayi1=Math.ceil(Math.random()*49)
-var sayi2=Math.ceil(Math.random()*49)
-var sayi3=Math.ceil(Math.random()*49)
-var sayi4=Math.ceil(Math.random()*49)
+function sayiUret(){
+    return Math.ceil(Math.random()*49)
+}
+
+var sayi =sayiUret()
+var sayi1=sayiUret()
+var sayi2=sayiUret()
+var sayi3=sayiUret()
+var sayi4=sayiUret()
+
 console.log(sayi+' '+sayi1+' '+sayi2+' '+sayi3+' '+sayi4)
