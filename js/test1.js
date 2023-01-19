0a38b6b60b680e335c9cb946abae51fe3eaa5bdf
Selin TANLA <rabiamercan8@gmail.com>
2023-01-19 22:06:53 +0300

    variables

---
+++ js/test1.js
@@ -0,0 +1,11 @@
+var isim='Tanla'
+console.log(isim)
+console.log(typeof(isim))
+
+var sayi =3
+console.log(sayi)
+console.log(typeof(sayi))
+
+var dogru=true
+console.log(dogru)
+console.log(typeof(dogru))
