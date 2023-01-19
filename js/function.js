b0d8ee5a6c1e6e2e077dacb68cf417ec9c0256b6
Selin TANLA <rabiamercan8@gmail.com>
2023-01-19 22:39:49 +0300

    parametre ile çalışmak

--- js/function.js
+++ js/function.js
@@ -1,13 +1,13 @@
 //random() : random sayı : 0 ve 1 arasında üretir bu yüzden 49 ile çarpıyorum
 //ceil() : sayıyı yukarı yuvarlar
-function sayiUret(){
-    return Math.ceil(Math.random()*49)
+function sayiUret(standart1 = 50){
+    return Math.ceil(Math.random()*standart1)
 }
 
 var sayi =sayiUret()
-var sayi1=sayiUret()
+var sayi1=sayiUret(89)
 var sayi2=sayiUret()
-var sayi3=sayiUret()
+var sayi3=sayiUret(10000)
 var sayi4=sayiUret()
 
 console.log(sayi+' '+sayi1+' '+sayi2+' '+sayi3+' '+sayi4)
