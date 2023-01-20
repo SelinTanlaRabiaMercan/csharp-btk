eec4900e6f7fea576991f4fde345cf7fb93cb172
Selin TANLA <rabiamercan8@gmail.com>
2023-01-20 09:57:19 +0300

    şart bloklarıyla en büyük sayı

---
+++ js/conditionals.js
@@ -0,0 +1,15 @@
+//En büyük sayıyı bulma
+
+var sayi1=67
+var sayi2=78
+var sayi3=94
+
+var enBuyuk = sayi1
+
+if(enBuyuk<sayi2){
+    enBuyuk=sayi2
+}if(enBuyuk<sayi3){
+    enBuyuk=sayi3
+}
+
+console.log("en büyük sayı "+enBuyuk)
